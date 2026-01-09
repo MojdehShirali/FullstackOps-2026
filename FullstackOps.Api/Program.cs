57aa6d835438cfaaacd04d920ee71be30872356d
Mahsa Shirali <mahsa1917@gmail.com>
2026-01-09 23:45:29 +0100

    feat(api): scaffold web api foundation

---
+++ FullstackOps.Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.MapGet("/__ping", () => Results.Ok(new { status = "ok", utc = DateTimeOffset.UtcNow }));
+app.MapHealthChecks("/health");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
